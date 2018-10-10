0ff30addd37bb2a91db36211fa39674fea9c775d
Cristina B <bau161260@uvg.edu.gt>
2018-10-09 18:53:42 -0600

    Primera prueba del main

---
+++ src/main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val boxOfChocolate = RanBoxOfChocolates<Chocolate>(arrayListOf<Chocolate> {
+    Chocolate("Hersheu")
+    });
+    val Forrest = Gump<Chocolate>(boxOfChocolate);
+}
